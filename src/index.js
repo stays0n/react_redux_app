967b260296de66c454416840e1496b789737c578
staysOn <risis666@gmail.com>
2022-01-13 19:03:26 +0300

    Practical task #1. Practice. Deleting a task

--- src/index.js
+++ src/index.js
@@ -1,14 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as actions from './store/actions';
+import { initiateStore } from './store/store';
+
+const store = initiateStore();
+
+const App = () => {
+    const [state, setState] = useState(store.getState());
+
+    useEffect(() => {
+        store.subscribe(() => setState(store.getState()));
+    }, []);
+
+    const completeTask = (taskId) => {
+        store.dispatch(actions.taskCompleted(taskId));
+    };
+
+    const changeTitle = (taskId) => {
+        store.dispatch(actions.titleChanged(taskId));
+    };
+
+    const deleteTask = (taskId) => {
+        store.dispatch(actions.taskDeleted(taskId));
+    } 
+
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+
+            <ul>
+                {state.map((el) => (
+                    <li key={el.id}>
+                        <p>{el.description}</p>
+                        <p>{el.title}</p>
+                        <p>{`Complited: ${el.completed}`}</p>
+                        <button onClick={() => completeTask(el.id)}>
+                            Complete
+                        </button>
+                        <button onClick={() => changeTitle(el.id)}>
+                            Change
+                        </button>
+                        <button onClick={() => deleteTask(el.id)}>
+                            Delete
+                        </button>
+                        <hr />
+                    </li>
+                ))}
+            </ul>
+        </React.Fragment>
+    );
+};
+
+export default App;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
